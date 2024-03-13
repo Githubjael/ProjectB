3b25551295f51608715e57be4a82fbf8819cdd15
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-03-13 14:54:37 +0100

    Update Reservations_Interactions.cs

--- Reservations_Interactions.cs
+++ Reservations_Interactions.cs
@@ -2,12 +2,14 @@ public class Reservation{
     public string Name;
     public string Date;
     public string Time;
+    public int SizeParty;
 
-    public Reservation(string name, string date, string time)
+    public Reservation(string name, string date, string time, int sizeParty)
     {
         Name = name;
         Date = date;
         Time = time;
+        SizeParty = sizeParty;
     }
 
     public void MakeReservation(){
