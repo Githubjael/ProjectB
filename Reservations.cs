52f6179130357807ecc8bb0debd9d32e3679a0d7
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-03-13 14:24:15 +0100

    Update Reservations.cs

--- Reservations.cs
+++ Reservations.cs
@@ -8,28 +8,28 @@ public class Reservations{
         Name = name;
     }
 
-    public void MakeReservation()
-    {
-        Console.WriteLine("Enter the desired date and time (in the format, '2024-03-15 19:00'): ");
-        string inputDateTime = Console.ReadLine();
+    // public void MakeReservation()
+    // {
+    //     Console.WriteLine("Enter the desired date and time (in the format, '2024-03-15 19:00'): ");
+    //     string inputDateTime = Console.ReadLine();
         
-        if (DateTime.TryParse(inputDateTime, out DateTime reservationDateTime))
-        {
-            if (!reservations.ContainsKey(reservationDateTime))
-            {
-                Console.Write("Voer uw naam in: ");
-                string guestName = Console.ReadLine();
-                reservations.Add(reservationDateTime, guestName);
-                Console.WriteLine($"Reservering op {reservationDateTime} voor {guestName} is gemaakt.");
-            }
-            else
-            {
-                Console.WriteLine("Deze tijd is al gereserveerd. Kies een andere tijd.");
-            }
-        }
-        else
-        {
-            Console.WriteLine("Ongeldige datum- en tijdnotatie. Probeer opnieuw.");
-        }
+    //     if (DateTime.TryParse(inputDateTime, out DateTime reservationDateTime))
+    //     {
+    //         if (!reservations.ContainsKey(reservationDateTime))
+    //         {
+    //             Console.Write("Voer uw naam in: ");
+    //             string guestName = Console.ReadLine();
+    //             reservations.Add(reservationDateTime, guestName);
+    //             Console.WriteLine($"Reservering op {reservationDateTime} voor {guestName} is gemaakt.");
+    //         }
+    //         else
+    //         {
+    //             Console.WriteLine("Deze tijd is al gereserveerd. Kies een andere tijd.");
+    //         }
+    //     }
+    //     else
+    //     {
+    //         Console.WriteLine("Ongeldige datum- en tijdnotatie. Probeer opnieuw.");
+    //     }
     }
 }
