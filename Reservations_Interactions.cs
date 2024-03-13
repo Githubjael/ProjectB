ce61450c13c4a90839c23234951454b837fa45ef
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-03-13 14:56:11 +0100

    Update Reservations_Interactions.cs

--- Reservations_Interactions.cs
+++ Reservations_Interactions.cs
@@ -15,6 +15,11 @@ public class Reservation{
     public void MakeReservation(){
         System.Console.WriteLine("Succesfully made the reservation!");
         //save it somewhere
+        //round sizeparty up to 2, 4 or 6
+        //if higher make it into multible reservations
+        //for example if sizeparty is 10 table is 6 and 4
+        //if sizeparty is 11 table is 6 and 6
+
 
     }
 }
