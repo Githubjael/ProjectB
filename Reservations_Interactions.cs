57c6d593505e8b03cb6f4b62f92cd84e3ac6f7b6
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-03-14 17:04:37 +0100

    Update Reservations_Interactions.cs

--- Reservations_Interactions.cs
+++ Reservations_Interactions.cs
@@ -1,16 +1,5 @@
-public class Reservation{
-    public string Name;
-    public string Date;
-    public string Time;
-    public int SizeParty;
-
-    public Reservation(string name, string date, string time, int sizeParty)
-    {
-        Name = name;
-        Date = date;
-        Time = time;
-        SizeParty = sizeParty;
-    }
+public class Reservation
+{
 
     public void MakeReservation()
     {
@@ -44,5 +33,5 @@ public class Reservation{
     
     }
 
-    }
+    
 }
