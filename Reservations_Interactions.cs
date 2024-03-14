633c63f5bbe060eda9f4834c1ae99146745a9a7a
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-03-14 20:53:02 +0100

    basic resevering (zonder object aanmaking of wat opslaan)

--- Reservations_Interactions.cs
+++ Reservations_Interactions.cs
@@ -1,37 +1,44 @@
-public class Reservation
+class Reservation
 {
+    public static new List<int> Month = new List<int>(){
+     1, 2, 3, 4, 5, 6, 7,
+     8, 9, 10, 11, 12, 13, 14,
+     15, 16, 17, 18, 19, 20, 21,
+     22, 23, 24, 25, 26, 27, 28,
+     29, 30, 31};
+    public static new List<string> AvailableHours = new List<string>(){"10:00", "10:30", "17:00", "18:00", "19:30", "20:00"};
+    public static void MakeReservation(){
+        // Vraag de user met hoeveel personen die gaat komen
+        Console.WriteLine("How many people are coming with you?");
+        int AmountOfGuests = Convert.ToInt32(Console.ReadLine());
 
-    public void MakeReservation()
-    {
-    
-        List<int> availableDays = new List<int>();
-    
-    
-        Console.WriteLine("How many people are you coming with?");
-        int guessAmount = Convert.ToInt32(Console.ReadLine());
-    
-    
-        Console.WriteLine("What month would you like to come?");
-        string monthChoice = Console.ReadLine();
-    
-    
-        Console.WriteLine($"These are the avalaible days in {monthChoice} for a reservation with {guessAmount} people:");
-        Console.WriteLine(availableDays);
-    
-        Console.WriteLine("What day would you like to come?");
-        int dayChoice = Convert.ToInt32(Console.ReadLine());
-    
-        Console.WriteLine("These are the available times to reservate a table on your chosen day:");
-        Console.WriteLine("available days implemented");
-    
-        Console.WriteLine("Chooce a tijdstip:");
-        string timeChoice = Console.ReadLine();
-    
-        Console.WriteLine("Do you confirm the reservation with the chosen date? y/n");
-        string reservationConfirmation = Console.ReadLine();
-    
-    
+        // Vraag de user in welke maan die wilt komen
+        Console.WriteLine("In what month would you like to book? Enter the number of that month.");
+        // We vragen de nummer van de maand om het voor even simpel te houden
+        int NumberOfMonth = Convert.ToInt32(Console.ReadLine());
+
+        // Beschikbare dagen van deze maand weergeven in een lijst
+        System.Console.WriteLine($"Available days for booking are:\n{string. Join(", ", Month)}.\nChoose a day.");
+        int ChosenDay = Convert.ToInt32(Console.ReadLine());
+
+        // Beschikbare tijden op gekozen dag:
+        System.Console.WriteLine($"Available hours for booking are:\n{string. Join(", ", AvailableHours)}\nChoose a time");
+        string ChosenTime = Console.ReadLine();
+
+        // Reserverings informatie weergeven ter bevestiging. User heeft nog GEEN recht op aanpassing
+        System.Console.WriteLine($"Your reservation details:\n{ChosenDay}/{NumberOfMonth}/2024 at {ChosenTime} for {AmountOfGuests} guests");
+        // Deze informatie gaan we later opslaan in een object
+
+        //Persoonlijke gegevens opvragen van user
+        System.Console.WriteLine("What is your phone number?");
+        string PhoneNumber = Console.ReadLine();
+
+        System.Console.WriteLine("What is your Email?");
+        string Email = Console.ReadLine();
+
+        // Later gaan we de user vragen om de reservering te  bevestigen maar voor nu even dit:
+        System.Console.WriteLine("Your reservation is confirmed. Your table ID = {RandomID}.\nThank you for choosing our restaurant, we look forward to serve you!");
+        // Hierna maken we een Reservation object aan met alle details van de reservation en stoppen we die in json
     }
-
-    
 }
+
