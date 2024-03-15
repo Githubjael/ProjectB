1b7e7cb1ef4a379272a7c99981e25e4046b021f9
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-03-15 16:10:39 +0100

    Update Reservation_data.cs

--- Reservation_data.cs
+++ Reservation_data.cs
@@ -1,1 +1,15 @@
+// indeze class worden de reservaties opgeslagen.
+public class ReservationsInfo
+{
+    private List<Reservation> reservations = new List<Reservation>();
 
+    public void AddReservation(Reservation reservation)
+    {
+        reservations.Add(reservation);
+    }
+
+    public List<Reservation> GetReservations()
+    {
+        return reservations;
+    }
+}
