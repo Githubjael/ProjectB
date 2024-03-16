cf1cbacf8201bc21560947cf0b4c808ba1a2b7d5
Githubjael <74548587+Githubjael@users.noreply.github.com>
2024-03-16 12:02:56 +0100

    i hope this was worth the wait

---
+++ Tables.cs
@@ -0,0 +1,14 @@
+class Tables
+{
+    public int ID;
+    public string Type;
+    public bool Reserved;
+    // public int Amount;
+    public Tables(int id, string type)
+    {
+        ID = id;
+        Type = type;
+        Reserved = false;
+        // Amount = amount;
+    }
+}
