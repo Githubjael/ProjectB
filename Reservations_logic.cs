bd1fbdab6555bd5d559035dbf5b18d6153ac475a
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-03-17 22:39:48 +0100

    Update Reservations_logic.cs

--- Reservations_logic.cs
+++ Reservations_logic.cs
@@ -1,1 +1,13 @@
+class ReservationLogic
+{
+    private static List<ReservationDataModel> _reservation = new List<ReservationDataModel>(){};
 
+    // voor nu adden we alleen maar reservations naar de prive lijst
+    public static void AddReservationToList(ReservationDataModel reservation)
+    {
+        _reservation.Add(reservation);
+        ReservationDataAccess.WriteToJson(_reservation);
+    }
+    // en deze lijst sturen we op een of andere manier naar json
+    // We roepen een method vanuit ReservationDataAccess (de write to json method)
+}
