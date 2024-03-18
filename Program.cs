1801d3aed9b1b13719c380f3cda25e478cc48ab6
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-03-18 14:03:40 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,28 +1,36 @@
+using System.Security.Cryptography.X509Certificates;
+
 public class Program
 {
     public static void Main()
     {
+        
         bool exit = false;
         //info abt restaurant, can be changed by manager
-        Console.WriteLine("add later");
+        RestaurantInfo resto1 = new RestaurantInfo("", "", 0);
+        string resto_info = resto1.GetInfo();
+        Console.WriteLine(resto_info);
+
 
         while (!exit)
         {
+            Console.WriteLine(resto_info);
             Console.WriteLine("[1] Make a reservation");
             Console.WriteLine("[2] View the menu");
             Console.WriteLine("[3] Exit");
+            Console.WriteLine("[4] Login as manager");
             Console.Write("Enter your choice: ");
             string choice = Console.ReadLine();
 
             switch (choice)
             {
                 case "1":
-                    Reservation.MakeReservation();
-                    foreach(var kvp in Reservation.tableAssignments)
-                    {
-                        // ik display even alle guestsIDS and table ids die bezet zijn // gwn een overzicht voor ons & omte checken of het werkt.
-                        Console.WriteLine($"GuestID: {kvp.Key}, TableID: {kvp.Value}");
-                    }
+                    // Reservation.MakeReservation();
+                    // foreach(var kvp in Reservation.tableAssignments)
+                    // {
+                    //     // ik display even alle guestsIDS and table ids die bezet zijn // gwn een overzicht voor ons & omte checken of het werkt.
+                    //     Console.WriteLine($"GuestID: {kvp.Key}, TableID: {kvp.Value}");
+                    // }
                     break;
 
                 case "2":
@@ -32,6 +40,45 @@ public class Program
                 case "3":
                     Console.WriteLine("Exited the program");
                     exit = true;  
+                    break;
+                
+                case "4":
+                //check login and password
+                    bool exit2 = false;
+                    while (!exit2)
+                    {
+                    Console.WriteLine("[1] Change the restaurant info");
+                    Console.WriteLine("[2] Change the menu"); // do later
+                    Console.WriteLine("[3] Exit");
+                    Console.Write("Enter your choice: ");
+                    string choice_manager = Console.ReadLine(); 
+                    
+                    switch (choice_manager)
+                    {
+                        case "1":
+                            Console.WriteLine("What is the current addres?");
+                            string restaurant_adress = Console.ReadLine();
+                            Console.WriteLine("What is the current email");
+                            string restaurant_email = Console.ReadLine();
+                            Console.WriteLine("What is the current phone number?");
+                            int phone_number = Convert.ToInt32(Console.ReadLine());
+                            resto1.Adress = restaurant_adress;
+                            resto1.Email = restaurant_email;
+                            resto1.Phone_number = phone_number;
+                            // string resto_info = $"Adress: {restaurant_adress}\nEmail: {email}\n Phone number: {phone_number}";
+
+                            break;
+                        
+                        case "2":
+                            break;
+                        case "3":
+                            Console.WriteLine("Exited the program");
+                            exit2 = true; 
+                            break;
+                        case "4":
+                            break;
+                    }
+                }
                     break;
 
                 default:
