b24352477e8b66b7277d86aa942f814b13e1259e
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-03-18 13:12:25 +0100

    Create MenuItems.cs

---
+++ MenuItems.cs
@@ -0,0 +1,13 @@
+public class MenuItem : Menu{
+    public string Name;
+    public double Price;
+
+
+    public MenuItem(string name, double price)
+    {
+        Name = name;
+        Price = price;
+    }
+
+
+}
