e480e8caba59b3744d0052a04d117b017979866b
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-03-18 14:11:56 +0100

    Update Reservations.cs

--- Reservations.cs
+++ Reservations.cs
@@ -15,7 +15,9 @@ public class Reservation
     // Zelfde geld voor AvailableHours, elke dag zal zijn eigen lijst hebben
     public static List<string> AvailableHours = new List<string>()
     {
-        "10:00", "10:30", "17:00", "18:00", "19:30", "20:00"
+        "10:00", "10:30", "11:00", "11:30", "12:00", "12:30", "13:00", "13:30", "14:00",
+        "14:30", "15:00", "15:30", "16:00", "16:30","17:00", "17:30", "18:00", "18:30",
+        "19:00", "19:30", "20:00", "20:30", "21:00", "21:30", "22:00", "22:30", "23:00"
     };
 
     public static List<int> unavailableTableIDs = new List<int>(); // als eerst maak ik een lijst om de gebruikte tafel IDs later op te slaan
