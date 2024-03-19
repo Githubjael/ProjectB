b64bf32e0222c69902e9875c2a3b9e09cfdaf083
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-03-19 23:05:47 +0100

    Update Reservations_logic.cs

--- Reservations_logic.cs
+++ Reservations_logic.cs
@@ -10,4 +10,51 @@ class ReservationLogic
     }
     // en deze lijst sturen we op een of andere manier naar json
     // We roepen een method vanuit ReservationDataAccess (de write to json method)
+
+
+
+    public static void CancelReservation(int guestID)
+    {
+        //ik maak beneden een methode om de reservatie te vinden die gekoppeld is aan guestiD
+        ReservationDataModel reservationToRemove = FindReservationByGuestID(guestID);
+
+        if (reservationToRemove != null)
+        {
+            //als eerst verwijder ik de reservatie van de list.
+            _reservation.Remove(reservationToRemove);
+
+            // ik sla de nieuwe lijst van reserveringen in de json op, dus de lijst zonder de gecancelde reservaties
+            ReservationDataAccess.WriteToJson(_reservation);
+
+            // ook voeg ik de tafel ID weer terug naar beschikbare tafels //daarvoor gebruik ik ff een korte methode (die ik beneden maak)
+            int tableID = reservationToRemove.TableID;
+            AddAvailableTable(tableID);
+
+            Console.WriteLine("Reservation is cancelled.");
+        }
+        else
+        {
+            Console.WriteLine("Reservation is not found.");
+        }
+    }
+
+    // korte methode om ff de rservatie op basis van guest ID te vinden.
+    private static ReservationDataModel FindReservationByGuestID(int guestID)
+    {
+        foreach (ReservationDataModel reservation in _reservation)  //deze loop gaat door de reservaties in de lijst , totdat hij de reservatie tegenkomt die gebonden is aan de guestID die hij uit de parameter krijgt.
+        {
+            if (reservation.GuestID == guestID)
+            {
+                return reservation;
+            }
+        }
+        return null; //null == geen reservatie gevonden
+    }
+
+    // ff weer een korte hulpmethode om de beschikbare tafels toe te voegen aan ons lijst van beschikbare tafels
+    private static void AddAvailableTable(int tableID)
+    {
+        string tableIDstring = Convert.ToString(tableID);
+        Reservation.AvailableTablesIDs.Add(tableIDstring);
+    }
 }
