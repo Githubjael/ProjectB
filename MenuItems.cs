2076bd0882190d6a73ae9e2d5fb9600227180383
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-03-20 11:59:36 +0100

    Update MenuItems.cs

--- MenuItems.cs
+++ MenuItems.cs
@@ -1,13 +1,24 @@
-public class MenuItem : Menu{
-    public string Name;
-    public double Price;
+public class MenuItem{
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public bool IsFish { get; set; }
+    public bool IsMeat { get; set; }
+    public bool IsVegetarian { get; set; }
+    public bool IsDrink { get; set; }
 
-
-    public MenuItem(string name, double price)
+    public MenuItem(string name, double price, bool isFish, bool isMeat, bool isVegetarian, bool isDrink)
     {
         Name = name;
         Price = price;
+        IsFish = isFish;
+        IsMeat = isMeat;
+        IsVegetarian = isVegetarian;
+        IsDrink = isDrink;
     }
+
+    //fish, meat vegan, vegatarian and drinks
+
+
 
 
 }
