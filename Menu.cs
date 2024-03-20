70dbaaf093eb6a2e6460c7510ff80e2d451e5ec8
DamianHRICT <146089069+DamianHRICT@users.noreply.github.com>
2024-03-20 11:59:25 +0100

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -4,7 +4,15 @@ public class Menu
 
     public Menu()
     {
-        Items = new List<MenuItem>();
+                Items = new List<MenuItem>
+        {
+            //bool order fish, meat, vegetarian, drink (use this to sort)
+            new MenuItem("Salmon", 15.99, true, false, false, false),
+            new MenuItem("Steak", 12.99, false, true, false, false),
+            new MenuItem("Caesar Salad", 8.99, false, false, true, false),
+            new MenuItem("Soda", 2.49, false, false, false, true),
+            new MenuItem("Iced Tea", 1.99, false, false, false, true)
+        };
     }
 
      public void AddItem(MenuItem item)
