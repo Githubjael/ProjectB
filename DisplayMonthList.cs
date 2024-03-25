a44697d5aad90305883563125ad21e99d43df57a
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-03-25 09:22:52 +0100

    Create DisplayMonthList.cs

---
+++ DisplayMonthList.cs
@@ -0,0 +1,44 @@
+class DisplayMonthList
+{
+    public static List<int> MonthList = new List<int>(){
+                1, 2, 3, 4, 5, 6, 7,
+                8, 9, 10, 11, 12, 13, 14,
+                15, 16, 17, 18, 19, 20, 21,
+                22, 23, 24, 25, 26, 27, 28,
+                29, 30, 31
+            };
+    public static List<int> GiveListBasedOnMonth(int Month)
+    {
+        if (Month == 1 || Month == 3 || Month == 5 || Month == 7 || Month == 8 || Month == 10 || Month == 12)
+        {
+            MonthList = new List<int>(){
+                1, 2, 3, 4, 5, 6, 7,
+                8, 9, 10, 11, 12, 13, 14,
+                15, 16, 17, 18, 19, 20, 21,
+                22, 23, 24, 25, 26, 27, 28,
+                29, 30, 31
+            };
+            return MonthList;
+        }
+        else if (Month == 2)
+        {
+            MonthList = new List<int>(){
+                1, 2, 3, 4, 5, 6, 7,
+                8, 9, 10, 11, 12, 13, 14,
+                15, 16, 17, 18, 19, 20, 21,
+                22, 23, 24, 25, 26, 27, 28
+            };
+            return MonthList;
+        }
+        else{
+            MonthList = new List<int>(){
+                1, 2, 3, 4, 5, 6, 7,
+                8, 9, 10, 11, 12, 13, 14,
+                15, 16, 17, 18, 19, 20, 21,
+                22, 23, 24, 25, 26, 27, 28, 29,
+                30
+            };
+            return MonthList;
+        }
+    }
+}
