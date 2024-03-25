d90d6c17b2fed96307146a54d046340ba7b15654
Mawaddahr <159782008+Mawaddahr@users.noreply.github.com>
2024-03-25 09:20:17 +0100

    Update ReservationDataModel.cs

--- ReservationDataModel.cs
+++ ReservationDataModel.cs
@@ -1,5 +1,6 @@
 using System.Text.Json.Serialization;
 class ReservationDataModel
+// [{Date: {"datum":{{}, {}, {}, {}}, {"datum": {{}, ...}}]
 {
     [JsonPropertyName("Guest ID")]
     public int GuestID {get; set;}
@@ -10,15 +11,25 @@ class ReservationDataModel
     [JsonPropertyName("Date")]
     public string Date {get; set;}
 
+    [JsonPropertyName("First name")]
+    public string FirstName {get; set;}
+
+    [JsonPropertyName("Last name")]
+
+    public string LastName {get; set;}
+
     [JsonPropertyName("Email address")]
     public string EmailAddress {get; set;}
 
     [JsonPropertyName("Phone number")]
     public string PhoneNumber {get; set;}
-    public ReservationDataModel(int guestID, int tableID, string date, string emailAddress, string phoneNumber)
+
+    public ReservationDataModel(int guestID, int tableID, string date, string firstName, string lastName, string emailAddress, string phoneNumber)
     {
         GuestID = guestID;
         TableID = tableID;
+        FirstName = firstName;
+        LastName = lastName;
         Date = date;
         EmailAddress = emailAddress;
         PhoneNumber = phoneNumber;
