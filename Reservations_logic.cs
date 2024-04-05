060effc5358b4349f4cc7774a5b96fb57422d72c
hilaldd <159782078+hilaldd@users.noreply.github.com>
2024-04-05 19:08:44 +0200

    Update Reservations_logic.cs

--- Reservations_logic.cs
+++ Reservations_logic.cs
@@ -2,7 +2,7 @@ public class ReservationLogic
 {
     public static List<ReservationDataModel> _reservation = new List<ReservationDataModel>(){};
 
-    //  voor nu adden we alleen maar reservations naar de prive lijst
+     //  voor nu adden we alleen maar reservations naar de prive lijst
     public static void AddReservationFromJson()
     {
         if (ReservationDataAccess.ReadFromJson() != null){
@@ -12,6 +12,8 @@ public class ReservationLogic
         }
     }
     }
+
+    //  voor nu adden we alleen maar reservations naar de prive lijst
     public static void AddReservationToList(ReservationDataModel reservation)
     {
         _reservation.Add(reservation);
@@ -20,24 +22,34 @@ public class ReservationLogic
     //  en deze lijst sturen we op een of andere manier naar json
     //  We roepen een method vanuit ReservationDataAccess (de write to json method)
 
+
     public static void CancelReservation(int guestID)
     {
-        //  ik maak beneden een methode om de reservatie te vinden die gekoppeld is aan guestiD
         ReservationDataModel reservationToRemove = FindReservationByGuestID(guestID);
 
         if (reservationToRemove != null)
         {
-        //  als eerst verwijder ik de reservatie van de list.
-            reservationToRemove.Table.Cancelled();
-            _reservation.Remove(reservationToRemove);
+            DateTime now = DateTime.Now;
+            DateTime reservationDateTime;
 
-        //   ik sla de nieuwe lijst van reserveringen in de json op, dus de lijst zonder de gecancelde reservaties
-            ReservationDataAccess.WriteToJson(_reservation);
+            if (DateTime.TryParse(reservationToRemove.Date + " " + reservationToRemove.Time, out reservationDateTime))
+            {
+                // ik controleer hoelang er nog voor de reservatie is/ dus of er nog minder dan 2 uur is / want dan is het annuleren niet meer mogelijk !
+                if ((reservationDateTime - now).TotalHours < 2)
+                {
+                    Console.WriteLine("Sorry, you can't cancel the reservation as it's less than 2 hours before/Past the reservation time. ");
+                    return;
+                }
 
-        //   ook voeg ik de tafel ID weer terug naar beschikbare tafels daarvoor gebruik ik ff een korte methode (die ik beneden maak)
-            string tableID = reservationToRemove.Table.ID;
-            // AddAvailableTable(tableID);
-            Console.WriteLine("Reservation is cancelled.");
+                // de rrservatie wordt geanulleerd 
+                _reservation.Remove(reservationToRemove);
+                ReservationDataAccess.WriteToJson(_reservation);
+                Console.WriteLine("Your Reservation is cancelled.");
+            }
+            else
+            {
+                Console.WriteLine("Error converting reservation time.");
+            }
         }
         else
         {
@@ -45,9 +57,12 @@ public class ReservationLogic
         }
     }
 
+
+
+
     //  korte methode om ff de reservatie op basis van guest ID te vinden.
     public static ReservationDataModel FindReservationByGuestID(int guestID)
-    {
+     {
         foreach (ReservationDataModel reservation in _reservation)  //deze loop gaat door de reservaties in de lijst , totdat hij de reservatie tegenkomt die gebonden is aan de guestID die hij uit de parameter krijgt.
         {
         if (reservation.GuestID == guestID)
@@ -58,7 +73,8 @@ public class ReservationLogic
         return null; //null == geen reservatie gevonden
     }
 
-    // check of tafel bezet is
+
+        // check of tafel bezet is
     public static bool CheckReservedTable(string ID, string Date, string Time)
     {
         foreach(ReservationDataModel reservation in _reservation)
